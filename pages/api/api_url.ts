0d4d4ae22813dd2932da3d80904044b4051b8cfb
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-11-03 22:42:25 -0300

    bug: slash for uri

--- pages/api/api_url.ts
+++ pages/api/api_url.ts
@@ -1,2 +1,2 @@
-export const api_url = "https://goa-portfolio-admin.herokuapp.com";
+export const api_url = "https://goa-portfolio-admin.herokuapp.com/";
 // export const api_url = "http://localhost:3333/";
